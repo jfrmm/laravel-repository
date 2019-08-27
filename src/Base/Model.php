d0c377516ddc126e86633a200e1a48d5259f1a77
Hugo Serra <hserra@alter-solutions.com>
2019-08-27 16:24:40 +0100

    :sparkles: Add Base Model with reflection for name

---
+++ src/Base/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ASP\Repository\Base;
+
+use ReflectionClass;
+use ReflectionException;
+
+class Model extends \Illuminate\Database\Eloquent\Model
+{
+    /**
+     * Get Model name through reflection
+     *
+     * @return string
+     * @throws ReflectionException
+     */
+    public function getModelName()
+    {
+        return (new ReflectionClass(self::class))->getShortName();
+    }
+}
