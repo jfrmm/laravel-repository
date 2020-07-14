2efb16d82d1514803d140c54b8997cb964fb56b7
Jorge Marcelino <jmarcelino@alter-solutions.com>
2020-07-14 16:51:40 +0100

    🔊 Add better Validation logs

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -3,6 +3,7 @@
 namespace ASP\Repository\Exceptions;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 /**
  * @package ASP\Repository\Exceptions
@@ -23,5 +24,26 @@ class ValidationException extends RepositoryException
          * https://laravel.com/docs/5.8/validation#quick-ajax-requests-and-validation
          */
         parent::__construct(null, Response::HTTP_UNPROCESSABLE_ENTITY, $message, $errors);
+    }
+
+    /**
+     * Report the exception to the log
+     *
+     * @return void
+     */
+    public function report()
+    {
+        $this->logValidationException();
+    }
+
+    /**
+     * Log this validation exception in a readable manner
+     *
+     * @return void
+     */
+    private function logValidationException()
+    {
+        Log::error($this->message);
+        Log::error(print_r($this->getExceptionData(), true));
     }
 }
